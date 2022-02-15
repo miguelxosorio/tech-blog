e80b4e752add2713b13209ce58591b3e6ad9a82a
vayne <miguel.osorio19@yahoo.com>
2022-02-15 16:55:33 -0600

    setting up connection to handlebars template

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+// importing express from node library
 const express = require('express');
+// creates an express application
 const app = express();
 
+// handlebars
+const exhbs = require('express-handlebars');
+const hbs = exhbs.create({});
+
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+// set up view engine
+app.set('view engine', 'handlebars');
+
+// main route
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('main');
 })
 
-app.listen(3001);
+// server connect
+app.listen(PORT, () => console.log(`Now listening to Port ${PORT}`));
