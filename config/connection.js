cba7d785ad9e88bd77ae8e3123a55dcdf1a3e54d
vayne <miguel.osorio19@yahoo.com>
2022-02-16 15:20:21 -0600

    setting up the dotenv, changed sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
+// initiate dotenv package - sensitive data won't be exposed
+require('dotenv').config();
 
 // create a connection to the database, pass in MySQL information for username and password
-const sequelize = new Sequelize('tech_blog_db', 'username', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
