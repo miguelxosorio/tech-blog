1b25605218f6cdad58d2eb3e922c605d6aba2eb1
vayne <miguel.osorio19@yahoo.com>
2022-02-17 17:35:24 -0600

    testing mock up, added files to support features, added dashboard handlebars and behavior, separated login and signup js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,31 +1,3 @@
-async function signupForm (event) {
-    event.preventDefault();
-
-    // assigning the values of the input into these variables, removing whitespaces
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    // if all these fields are true, make the post request
-    if(username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        // check response status
-        if(response.ok) {
-            console.log('success');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
 async function loginForm (event) {
     event.preventDefault();
 
@@ -51,4 +23,3 @@ async function loginForm (event) {
 }
 
 document.querySelector('.login-form').addEventListener('submit', loginForm);
-document.querySelector('.signup-form').addEventListener('submit', signupForm);
