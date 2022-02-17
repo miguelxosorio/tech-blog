e8e568b9e7b60788379346954e8a6e08bd389436
vayne <miguel.osorio19@yahoo.com>
2022-02-17 15:06:06 -0600

    added logout button and behavior

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -148,4 +148,15 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+// logout route
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 module.exports = router;
