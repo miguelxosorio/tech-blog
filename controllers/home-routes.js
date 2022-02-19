a6839e99804409698b8b171a5f5641845984a736
vayne <miguel.osorio19@yahoo.com>
2022-02-18 19:29:08 -0600

    rewriting the login js and login handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,69 +3,9 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-   Post.findAll({
-       attributes: ['id', 'title', 'content', 'created_at'],
-       include: [
-           {
-               model: Comment,
-               attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
-               include: {
-                   model: User,
-                   attributes: ['username']
-               }
-           },
-           {
-               model: User,
-               attributes: ['username']
-           }
-       ]
-   })
-   .then(dbPostData => {
-        // This will loop over and map each Sequelize object into a serialized version of itself, saving the results in a new posts array
-       const posts = dbPostData.map(post => post.get({ plain: true }));
-       // pass post to object into the homepage template
-       // use this with the #each helper in the homepage.handlebars to render the posts
-       // in the homepage any HTML code between the <li> will be repeated for every item in posts
-       res.render('homepage', { 
-           posts,
-           loggedIn: req.session.loggedIn 
-        });
-   })
-   .catch(err => {
-       console.log(err);
-       res.status(500).json(err);
-   });
-});
-
-router.get('/login', (req, res) => {
-    if(req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login')
-});
-
-// separate signup route
-router.get('/signup', (req, res) => {
-    // signup handlebar
-    res.render('signup');
-})
-
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: ['id', 'title', 'content', 'created_at'],
+    Post.findAll({
+        attributes: ['id', 'content', 'title', 'created_at'],
         include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
             {
                 model: User,
                 attributes: ['username']
@@ -73,25 +13,20 @@ router.get('/post/:id', (req, res) => {
         ]
     })
     .then(dbPostData => {
-        if(!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id' });
-            return;
-        }
-        
-        // serialize the data
-        const post = dbPostData.get({ plain: true });
-
-        // pass data to template
-        res.render('single-post', {
-            // to pass a session variable to the template
-            post,
-            loggedIn: req.session.loggedIn 
-        });
+        // The data that Sequelize returns is actually a Sequelize object with a lot more information attached to it than you might have been expecting.
+        // To serialize the object down to only the properties you need, you can use Sequelize's get() method
+        // need the entire array of posts to be in the template, need to serialize entire array
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
     })
     .catch(err => {
-        console.log(err);
+        console.error(err);
         res.status(500).json(err);
     });
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+})
 
 module.exports = router;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -15,11 +15,41 @@ async function loginForm (event) {
         });
 
         if(response.ok) {
-            document.location.replace('/');
+            // if logged in, reroute to homepage
+            document.location.replace('/'); // change to /dashboard?
         } else {
             alert(response.statusText);
         }
     }
 }
 
+async function signupForm (event) {
+    event.preventDefault();
+
+    // assigning the values of the input into these variables, removing whitespaces
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    // if all these fields are true, make the post request
+    if(username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        // check response status
+        if(response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupForm);
 document.querySelector('.login-form').addEventListener('submit', loginForm);
